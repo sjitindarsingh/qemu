33237a06437cc7e2cc21c3e9ac9df0afb9795a21
Serge Hallyn <serge.hallyn@ubuntu.com>
2013-10-22 22:47:07 -0500

    [PATCH 03/71] sd: Allow sd_init callers to specify whether card is
    Gbp-Pq: linaro-patches/0003-sd-Allow-sd_init-callers-to-specify-whether-card-is-.patch.

--- hw/sd/milkymist-memcard.c
+++ hw/sd/milkymist-memcard.c
@@ -254,7 +254,7 @@ static int milkymist_memcard_init(SysBusDevice *dev)
     DriveInfo *dinfo;
 
     dinfo = drive_get_next(IF_SD);
-    s->card = sd_init(dinfo ? dinfo->bdrv : NULL, false);
+    s->card = sd_init(dinfo ? dinfo->bdrv : NULL, false, false);
     s->enabled = dinfo ? bdrv_is_inserted(dinfo->bdrv) : 0;
 
     memory_region_init_io(&s->regs_region, OBJECT(s), &memcard_mmio_ops, s,
