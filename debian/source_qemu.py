481357853974986cd0f7d712048757673d358b03
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-08-05 13:53:06 -0500

    Import patches-unapplied version 2.1+dfsg-2ubuntu1 to ubuntu/utopic-proposed
    Imported using git-ubuntu import.
    
    Changelog parent: 5429f4e8161b002131ebb07f058505b62a4150fc
    
    New changelog entries:
      * Merge new debian release
      * Remaining changes:
        - qemu-system-x86.links: add eepro100.rom link, drop links which we
          have in ipxe-qemu package.
        - control-in: stick to libsdl1.2-dev.
        - qemu-system-common.install: add debian/tmp/usr/lib to install the
          qemu-bridge-helper
        - qemu-system-common.postinst: remove acl placed by udev,
          and add udevadm trigger.
        - qemu-system-common.preinst: add kvm group if needed
        - add qemu-kvm upstart job and defaults file (rules,
          qemu-system-x86.qemu-kvm.default, qemu-system-x86.qemu-kvm.upstart)
        - debian/rules: add qemu-kvm-spice
        - rules,qemu-system-x86.modprobe: support use under older udevs which
          do not auto-load the kvm kernel module.  Enable nesting by default
          on intel.
        - binfmt-update-in: make sure to filter out compat arches.
        - debian/qemu-system-alternatives.in: use a later version as ubuntu
          removed the alternatives bit later.
        - d/p/ubuntu/expose-vmx_qemu64cpu.patch: enable nested kvm by default
          in qemu64 cpu type.
        - d/p/ubuntu/define-trusty-machine-type.patch: define a default trusty
          machine type to ease future live vm migration.
        - apport hook for qemu source package: d/source_qemu-kvm.py,
          d/qemu-system-common.install

---
+++ debian/source_qemu.py
@@ -0,0 +1,13 @@
+'''apport package hook for qemu
+
+(c) 2009 Canonical Ltd.
+Author: Dustin Kirkland <kirkland@canonical.com>
+'''
+
+from apport.hookutils import *
+
+def add_info(report):
+    attach_hardware(report)
+    attach_related_packages(report, ['kvm*', '*libvirt*', 'virt-manager', 'qemu*'])
+    report['KvmCmdLine'] = command_output(['ps', '-C', 'kvm', '-F'])
+    report['KvmCmdLine'] = command_output(['ps', '-C', 'qemu', '-F'])
