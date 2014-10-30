3632e4c602dfbfaae3391e4c1e7184734c7646d8
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-10-29 22:31:43 -0500

    use (arch-specific) data path, not just a single directory
    Gbp-Pq: use-arch-data-path.patch.

--- vl.c
+++ vl.c
@@ -124,7 +124,8 @@ int main(int argc, char **argv)
 #define MAX_VIRTIO_CONSOLES 1
 #define MAX_SCLP_CONSOLES 1
 
-static const char *data_dir[16];
+extern char qemu_datapath[]; /* arch-specific */
+static const char *data_dir[32];
 static int data_dir_idx;
 const char *bios_name = NULL;
 enum vga_retrace_method vga_retrace_method = VGA_RETRACE_DUMB;
@@ -4051,9 +4052,12 @@ int main(int argc, char **argv, char **envp)
         }
     }
 
-    /* If all else fails use the install path specified when building. */
-    if (data_dir_idx < ARRAY_SIZE(data_dir)) {
-        data_dir[data_dir_idx++] = CONFIG_QEMU_DATADIR;
+    /* add standard and arch-specific dirs to data path */
+    for(optarg = strtok(qemu_datapath, ":");
+        optarg && data_dir_idx < ARRAY_SIZE(data_dir);
+        optarg = strtok(NULL, ":"))
+    {
+        data_dir[data_dir_idx++] = optarg;
     }
 
     smp_parse(qemu_opts_find(qemu_find_opts("smp-opts"), NULL));
