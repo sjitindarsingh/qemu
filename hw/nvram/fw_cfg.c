a440d35aa2224a95c55f70449c7b417241ea6ba4
James Page <james.page@ubuntu.com>
2016-02-24 17:59:13 +0000

    fw_cfg: add check to validate current entry value
    Gbp-Pq: fw_cfg-add-check-to-validate-current-entry-value-CVE-2016-1714.patch.

--- hw/nvram/fw_cfg.c
+++ hw/nvram/fw_cfg.c
@@ -270,7 +270,8 @@ static int fw_cfg_select(FWCfgState *s, uint16_t key)
 static uint8_t fw_cfg_read(FWCfgState *s)
 {
     int arch = !!(s->cur_entry & FW_CFG_ARCH_LOCAL);
-    FWCfgEntry *e = &s->entries[arch][s->cur_entry & FW_CFG_ENTRY_MASK];
+    FWCfgEntry *e = (s->cur_entry == FW_CFG_INVALID) ? NULL :
+                     &s->entries[arch][s->cur_entry & FW_CFG_ENTRY_MASK];
     uint8_t ret;
 
     if (s->cur_entry == FW_CFG_INVALID || !e->data || s->cur_offset >= e->len)
