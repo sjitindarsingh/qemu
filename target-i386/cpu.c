0169b68da81df2ea77e12186e9e1677ebf04ded9
Marc Deslauriers <marc.deslauriers@ubuntu.com>
2018-05-23 08:01:27 -0400

    [PATCH v3 2/5] i386: define the AMD 'virt-ssbd' CPUID feature bit
    Gbp-Pq: CVE-2018-3639-2.patch.

--- target-i386/cpu.c
+++ target-i386/cpu.c
@@ -295,7 +295,7 @@ static const char *cpuid_8000_0008_ebx_feature_name[] = {
     "ibpb", NULL, NULL, NULL,
     NULL, NULL, NULL, NULL,
     NULL, NULL, NULL, NULL,
-    NULL, NULL, NULL, NULL,
+    NULL, "virt-ssbd", NULL, NULL,
     NULL, NULL, NULL, NULL,
 };
 
