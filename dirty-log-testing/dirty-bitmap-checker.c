28281a8e2cab42eb22c38cbc092044d703962509
Suraj Jitindar Singh <sjitindarsingh@gmail.com>
2018-12-17 11:48:09 +1100

    simplify dirty-bitmap-checker.c

--- dirty-log-testing/dirty-bitmap-checker.c
+++ dirty-log-testing/dirty-bitmap-checker.c
@@ -5,9 +5,20 @@
 #include <errno.h>
 #include <error.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define MIN(a,b) ((a) < (b) ? a : b)
-#define MBUF_SZ (4 << 20)
+
+#undef DEBUG
+#ifdef DEBUG
+#define pr_debug printf
+#else
+#define pr_debug(...) do { } while(0)
+#endif
 
 void usage(char *prog_name)
 {
@@ -15,182 +26,111 @@ void usage(char *prog_name)
            prog_name);
 }
 
-static uint64_t get_shift(uint64_t pgsz)
+static bool generate_pg_dirty(uint8_t *a, uint8_t *b, uint64_t pgsz)
 {
-    int page_shift = 0;
+    int i;
 
-    pgsz >>= 1;
-    while (pgsz) {
-        pgsz >>= 1;
-        page_shift++;
+    for (i = 0; i < pgsz; i++) {
+	    if (a[i] != b[i])
+		    return true;
     }
 
-    return page_shift;
+    return false;
 }
 
-static void
-generate_block(uint8_t *a, uint8_t *b, uint8_t *bm, size_t count,
-               uint64_t pgsz, uint64_t addr, uint64_t *dirty_count)
-{
-    uint64_t *a_ptr = (uint64_t *)a;
-    uint64_t *b_ptr = (uint64_t *)b;
-    size_t i, j;
-    int pgshift = get_shift(pgsz);
-    int idxshift = get_shift(sizeof(uint64_t));
-    bool page_dirty = false;
-
-    /* pages / addresses are relative to block. 'addr' is just for reporting */
-    for (i = 0; i < count / sizeof(uint64_t); i++) {
-        /* TODO: if page is dirty we can skip to the next and not continue scanning */
-        if (((i << idxshift) % pgsz) == 0) {
-            page_dirty = false;
-        }
-        if (a_ptr[i] != b_ptr[i]) {
-            size_t page_num = (i << idxshift) >> pgshift;
-            uint8_t bm_bit_pos = (page_num % 8);
-
-            if (page_dirty == false) {
-                page_dirty = true;
-                bm[page_num >> 3] |= (1 << bm_bit_pos);
-                *dirty_count += 1;
-            }
-        }
-    }
-}
-
-static bool
-check_block(uint8_t *a, uint8_t *b, uint8_t *bm, size_t count,
-            uint64_t pgsz, uint64_t addr, uint64_t *dirty_count,
-            bool exit_on_mismatch)
-{
-    size_t i, j, skip;
-    int pgshift = get_shift(pgsz);
-    int idxshift = get_shift(sizeof(uint64_t));
-    bool page_dirty;
-    bool mismatch = false;
-
-    uint64_t *a_ptr = (uint64_t *)a;
-    uint64_t *b_ptr = (uint64_t *)b;
-
-    for (i = 0; i < count / sizeof(uint64_t) && (!mismatch || !exit_on_mismatch); i += skip) {
-        skip = 1;
-        if (((i << idxshift) % pgsz) == 0) {
-            page_dirty = false;
-        }
-        if (a_ptr[i] != b_ptr[i]) {
-            size_t page_num = (i << idxshift) >> pgshift;
-            uint8_t bm_byte = bm[page_num >> 3];
-            uint8_t bm_bit_pos = (page_num % 8);
-            bool bm_bit;
-
-            if (page_dirty == false) {
-                page_dirty = true;
-                *dirty_count += 1;
-            }
-
-            if (bm_byte == 0) {
-                printf("page at addr 0x%lx is dirty, but byte 0x%lx of bitmap is clear\n",
-                       (addr + (i << idxshift)),
-                       ((addr + (i << idxshift)) >> pgshift) >> 3);
-                mismatch = true;
-            }
-
-            bm_bit = !!(bm_byte & (1 << bm_bit_pos));
-            if (bm_byte != 0 && !bm_bit) {
-                printf("page at addr 0x%lx is dirty, but bit %u of byte 0x%lx (0x%x) of bitmap is clear\n",
-                       (addr + (i << idxshift)),
-                       bm_bit_pos,
-                       ((addr + (i << idxshift)) >> pgshift) >> 3,
-                       bm_byte);
-                mismatch = true;
-            }
-        }
-        if (page_dirty)
-            skip = (pgsz - ((i << idxshift) % pgsz)) / sizeof(uint64_t);
-    }
-
-    return !mismatch;
-}
-
-void generate_bitmap(FILE *a, FILE *b, FILE *bm,
+void generate_bitmap(uint8_t *a, uint8_t *b, uint8_t *bm,
                      uint64_t maddr, uint64_t msize, uint64_t pgsz)
 {
-    bool last_loop = false;
-    uint64_t mpos, dirty_count = 0;
-    uint8_t *a_buf, *b_buf, *bm_buf;
+    uint64_t mpos;
 
-    mpos = maddr;
-    a_buf = calloc(1, MBUF_SZ);
-    b_buf = calloc(1, MBUF_SZ);
-    bm_buf = calloc(1, MBUF_SZ / pgsz);
+    for (mpos = 0; mpos < msize; mpos += pgsz) {
+	int64_t pgnum = mpos / pgsz;
+	bool pg_dirty;
 
-    while (!last_loop && mpos < (maddr + msize)) {
-        size_t acount, bcount, bmcount, count;
-        acount = fread(a_buf, 1, MBUF_SZ, a);
-        if (acount < MBUF_SZ)
-            last_loop = true;
-        bcount = fread(b_buf, 1, MBUF_SZ, b);
-        if (bcount < MBUF_SZ)
-            last_loop = true;
+	pg_dirty = generate_pg_dirty(&a[mpos], &b[mpos], pgsz);
 
-        bmcount = MBUF_SZ / pgsz / 8;
-        memset(bm_buf, 0, bmcount);
-
-        count = MIN(MIN(acount, bcount), bmcount * pgsz * 8);
-        generate_block(a_buf, b_buf, bm_buf, count, pgsz, mpos, &dirty_count);
-        fwrite(bm_buf, 1, bmcount, bm);
-
-        mpos += MBUF_SZ;
+	bm[pgnum / 8] &= ~(1UL << (pgnum % 8));
+	bm[pgnum / 8] |= (!!pg_dirty) << (pgnum % 8);
     }
 }
 
-bool check_bitmap(FILE *a, FILE *b, FILE *bm,
+static bool check_page(uint8_t *a, uint8_t *b, bool bm,
+			uint64_t pgsz, uint64_t addr, uint64_t *dirty_count)
+{
+    int i;
+    bool page_dirty = false;
+
+    pr_debug("Page at 0x%lx expected to be %s\n", addr, bm ? "dirty" : "clean");
+
+    page_dirty = !!memcmp(a, b, pgsz);
+    if (!page_dirty) {
+        if (bm)
+            pr_debug("page at addr 0x%lx is clean, but bitmap is dirty\n", addr);
+        return false;
+    } else if (page_dirty && bm) {
+        /* page is dirty */
+        *dirty_count += 1;
+        return false;
+    }
+
+    /* Page dirty and !bm */
+    for (i = 0; i < pgsz; i++) {
+	if (a[i] != b[i]) {
+		/* page is dirty */
+		*dirty_count += 1;
+
+                printf("page at addr 0x%lx (0x%lx) is dirty, but bitmap is clean\n",
+                        addr, addr + i);
+                printf("a 0x%x b 0x%x bm %d\n", a[i], b[i], bm);
+                return true;
+	}
+    }
+}
+
+bool check_bitmap(void *a, void *b, void *bm,
                   uint64_t maddr, uint64_t msize, uint64_t pgsz, bool exit_on_mismatch)
 {
-    bool last_loop;
     uint64_t mpos, dirty_count = 0;
-    uint8_t *a_buf, *b_buf, *bm_buf;
     bool mismatch = false;
 
-    mpos = maddr;
-    a_buf = calloc(1, MBUF_SZ);
-    b_buf = calloc(1, MBUF_SZ);
-    bm_buf = calloc(1, MBUF_SZ / pgsz);
+    for (mpos = 0; (mpos < msize) && (!mismatch || !exit_on_mismatch);
+		       mpos += pgsz) {
+	uint64_t count = MIN(pgsz, (msize - mpos));
+	int64_t ret, pg_num;
+        bool pg_mismatch;
+	uint8_t bmap_byte;
+	bool bmap_bit;
 
-    while (!last_loop && mpos < (maddr + msize) && (!mismatch || !exit_on_mismatch)) {
-        size_t acount, bcount, bmcount, count;
-        bool block_mismatch;
-        acount = fread(a_buf, 1, MBUF_SZ, a);
-        if (acount < MBUF_SZ)
-            last_loop = true;
-        bcount = fread(b_buf, 1, MBUF_SZ, b);
-        if (bcount < MBUF_SZ)
-            last_loop = true;
-        bmcount = fread(bm_buf, 1, MBUF_SZ / pgsz / 8, bm);
-        if (bmcount < MBUF_SZ / pgsz / 8)
-            last_loop = true;
+	pg_num = mpos / pgsz;
+	bmap_byte = ((uint8_t *) bm)[pg_num / 8];
+	bmap_bit = !!(bmap_byte & (1UL << (pg_num % 8)));
 
-        count = MIN(MIN(acount, bcount), bmcount * pgsz * 8);
-        block_mismatch = !check_block(a_buf, b_buf, bm_buf, count, pgsz, mpos, &dirty_count, exit_on_mismatch);
-        if (block_mismatch) {
+	/* Check for mismatches */
+        pg_mismatch = check_page(&(((uint8_t *) a)[mpos]), &(((uint8_t *) b)[mpos]),
+				bmap_bit, count, mpos, &dirty_count);
+        if (pg_mismatch) {
             mismatch = true;
-            printf("inconsistent dirty bitmap within 0x%lx to 0x%lx\n", mpos, mpos + MBUF_SZ);
+            printf("inconsistent dirty bitmap within 0x%lx to 0x%lx\n", mpos, mpos + pgsz);
+	    pr_debug("pg_num %lu, bmap_byte %lu (%x), bmap_bit %u (%d)\n",
+			    pg_num, pg_num / 8, bmap_byte,
+			    pg_num % 8, bmap_bit);
         }
-        mpos += MBUF_SZ;
     }
 
     if (!mismatch)
         printf("no inconsistencies found. found %lu dirty pages.\n", dirty_count);
+    else
+	printf("!!!mismatch!!!. found %lu dirty pages.\n", dirty_count);
 
-    return !mismatch;
+    return mismatch;
 }
-
 
 int main(int argc, char **argv)
 {
     const char *a_path, *b_path, *bm_path;
     uint64_t maddr, msize, pgsz;
-    FILE *a, *b, *bm;
+    int a_fd, b_fd, bm_fd, ret;
+    void *a, *b, *bm;
     bool generate = false;
     bool exit_on_mismatch = true, mismatch = false;
 
@@ -218,38 +158,53 @@ int main(int argc, char **argv)
     if (argc > 6)
         pgsz = atol(argv[6]);
     else
-        pgsz = 4096;
+        pgsz = 4096; /* This is hard coded in qemu */
 
     if (argc > 7)
         error(1, 0, "just use all the same starting offsets for now");
 
-    a = fopen(a_path, "rb");
-    if (!a)
+    /* Open input files */
+    a_fd = open(a_path, O_RDONLY);
+    if (a_fd < 0)
         error(1, errno, "failed to open %s", a_path);
-    b = fopen(b_path, "rb");
-    if (!b)
+    b_fd = open(b_path, O_RDONLY);
+    if (b_fd < 0)
         error(1, errno, "failed to open %s", b_path);
-    bm = fopen(bm_path, generate ? "wb" : "rb");
-    if (!bm)
-        error(1, errno, "failed to open %s", argv[3]);
+    bm_fd = open(bm_path, generate ? O_RDWR : O_RDONLY);
+    if (bm_fd < 0)
+        error(1, errno, "failed to open %s", bm_path);
 
-    if (maddr != 0) {
-        if (fseek(a, maddr, SEEK_SET))
-            error(1, errno, "%s: seek to addr 0x%lx failed", a_path, maddr);
-        if (fseek(b, maddr, SEEK_SET))
-            error(1, errno, "%s: seek to addr 0x%lx failed", b_path, maddr);
-        if (fseek(bm, maddr / pgsz / 8, SEEK_SET))
-            error(1, errno, "%s: seek to addr 0x%lx failed", bm_path, maddr / pgsz / 8);
+    /* mmap input files */
+    a = mmap(NULL, msize, PROT_READ, MAP_SHARED, a_fd, maddr);
+    if (a < 0)
+	    error(1, errno, "failed to mmap %s", a_path);
+    b = mmap(NULL, msize - maddr, PROT_READ, MAP_SHARED, b_fd, maddr);
+    if (b < 0)
+	    error(1, errno, "failed to mmap %s", b_path);
+    bm = mmap(NULL, msize / (pgsz * 8), generate ? PROT_WRITE | PROT_READ : PROT_READ
+		  , MAP_SHARED, bm_fd, (maddr / (pgsz * 8)));
+    if (bm < 0)
+	    error(1, errno, "failed to mmap %s", bm_path);
+
+    if (generate) {
+        generate_bitmap((uint8_t *) a, (uint8_t *) b, (uint8_t *) bm, maddr, msize, pgsz);
+    } else {
+	pr_debug("Checking for mismatches from 0x%lx->0x%lx\n", maddr, maddr + msize);
+        mismatch = check_bitmap(a, b, bm, maddr, msize, pgsz, exit_on_mismatch);
     }
 
-    if (generate)
-        generate_bitmap(a, b, bm, maddr, msize, pgsz);
-    else
-        mismatch = !check_bitmap(a, b, bm, maddr, msize, pgsz, exit_on_mismatch);
-
-    fclose(a);
-    fclose(b);
-    fclose(bm);
+    ret = munmap(a, msize);
+    if (ret)
+	    error(1, errno, "failed to munmap %s\n", a_path);
+    ret = munmap(b, msize);
+    if (ret)
+	    error(1, errno, "failed to munmap %s\n", b_path);
+    ret = munmap(bm, msize / (pgsz * 8));
+    if (ret)
+	    error(1, errno, "failed to munmap %s\n", bm_path);
+    close(a_fd);
+    close(b_fd);
+    close(bm_fd);
 
     return (mismatch) ? 1 : 0;
 }
